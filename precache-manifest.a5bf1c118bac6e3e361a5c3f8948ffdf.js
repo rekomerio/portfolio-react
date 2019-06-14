010e699dfdf29136914b8d67d3a3516e54c375b9
K9260 <K9260@student.jamk.fi>
2019-06-14 22:49:51 +0300

    Updates

---
+++ precache-manifest.a5bf1c118bac6e3e361a5c3f8948ffdf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aec25ef88db5ce9062ffa3e0acd47375",
+    "url": "/portfolio-react/index.html"
+  },
+  {
+    "revision": "0870d19ff3ce0caaffda",
+    "url": "/portfolio-react/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/portfolio-react/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "0870d19ff3ce0caaffda",
+    "url": "/portfolio-react/static/js/main.2e11c261.chunk.js"
+  },
+  {
+    "revision": "11528ddda4154ce1e32b",
+    "url": "/portfolio-react/static/js/runtime~main.72b3206a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
