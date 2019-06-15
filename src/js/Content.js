7e3cf6810478fc7a306453e447c10554ed90990c
K9260 <K9260@student.jamk.fi>
2019-06-15 15:17:39 +0300

    Decent layout coming out

--- src/js/Content.js
+++ src/js/Content.js
@@ -3,7 +3,18 @@ import React from 'react';
 function Content() {
     return (
         <div className="content">
-
+            <div>
+                <h1>Hello</h1>
+                <p>Testing</p>
+            </div>
+            <div>
+                <h1>Hello</h1>
+                <p>Testing</p>
+            </div>
+            <div>
+                <h1>Hello</h1>
+                <p>Testing</p>
+            </div>
         </div>
     );
 }
